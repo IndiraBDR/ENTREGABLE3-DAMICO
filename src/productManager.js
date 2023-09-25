3894c6bb24d43c3bb87fa75bc6ff28f1051f6b4a
IndiraBDR <indiradamico7@gmail.com>
2023-09-25 10:23:22 -0300

    detalles

--- src/productManager.js
+++ src/productManager.js
@@ -107,4 +107,3 @@ class ProductManager {
 }
 
 export { ProductManager };
-
--- src/server.js
+++ src/server.js
@@ -1,84 +1,42 @@
 import express from "express";
 import { ProductManager } from "./productManager.js";
 
+const app = express();
 
-const app = express()
-
-const productManager = new ProductManager(); //instancia de la class
-
-/*
-app.get('/api/products', async (req,res)=>{
-
-    let limit =  Number(req.query.limit)  ;
-    let products= await productManager.getProduct();
-
-    if(limit){
-
-        let productsLimitados=  products.slice(0,limit)
-     
-        res.json(productsLimitados)
-    }else{
-
-
-        res.json(products)
-    };
-
-  
-
-})
-
-app.get('/api/products/:id', async (req,res)=>{
-
-   const {id} = req.params ;
-  
-  let productoFiltrado= await productManager.getProductById(+id);
-  
-  res.json(productoFiltrado)
-
-})
-
-*/
+const productManager = new ProductManager();
 
 app.get("/api/products", async (req, res) => {
-    //let limit =  Number(req.query.limit)  ;
-    const { limit } = req.query;
-  
-    try {
-      let products = await productManager.getProduct();
-  
-      if (limit) {
-        let productsLimitados = products.slice(0, +limit);
-  
-        res.status(200).json({ message: "product found", productsLimitados });
-      } else {
-        res.status(200).json({ message: "product total", products });
-      }
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+  const { limit } = req.query;
+
+  try {
+    let products = await productManager.getProduct();
+
+    if (limit) {
+      let productsLimitados = products.slice(0, +limit);
+
+      res.status(200).json({ message: "product found", productsLimitados });
+    } else {
+      res.status(200).json({ message: "product total", products });
     }
-  });
-  
-  app.get("/api/products/:id", async (req, res) => {
-    const { id } = req.params;
-  
-    try {
-      let productoFiltrado = await productManager.getProductById(+id);
-  
-      if (!productoFiltrado) {
-        res.status(404).json({ message: "product not found" });
-      } else {
-        res.status(200).json({ message: "product found", productoFiltrado });
-      }
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let productoFiltrado = await productManager.getProductById(+id);
+
+    if (!productoFiltrado) {
+      res.status(404).json({ message: "product not found" });
+    } else {
+      res.status(200).json({ message: "product found", productoFiltrado });
     }
-  });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-
-
-app.listen(8080,()=>{
-
-   
-
-
-})
+app.listen(8080, () => {});
